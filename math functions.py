48e3aa00f9f85a8bcdcfa2ca0cebe503baef8086
Dell <trustever.jc@gmail.com>
2023-10-23 04:25:59 -0700

    another way of multiple assignment of variables in a single line of code

--- math functions.py
+++ math functions.py
@@ -14,3 +14,6 @@ y=2
 z=3
 print("The max no among the three varibles is: "+ str(max(x,y,z)))
 print("The min no among the three varibles is: "+str(min(x,y,z)))
+#another way of multiple assignment of variables in a single line of code
+name,age,attractive="Aman",22,True
+print(name,age,attractive)
