a45d0b1e7f1119e9714e01738fab0d94c7eb4dc1
Dell <trustever.jc@gmail.com>
2023-10-22 22:41:06 -0700

    input data from a user and casting datatype

---
+++ input data.py
@@ -0,0 +1,6 @@
+name=input("what is your name?: ")
+age=int(input("how old are you?: "))
+height=float(input("How tall are you?: "))
+print("your name is: "+name)
+print("your name is: "+str(age))
+print("your name is: "+str(height)+"cm")
