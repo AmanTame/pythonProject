750722f2c307cd6f5bbee2a1f5d8f73b58de383f
Dell <trustever.jc@gmail.com>
2023-10-23 04:51:00 -0700

    string methods

--- math functions.py
+++ math functions.py
@@ -15,5 +15,5 @@ z=3
 print("The max no among the three varibles is: "+ str(max(x,y,z)))
 print("The min no among the three varibles is: "+str(min(x,y,z)))
 #another way of multiple assignment of variables in a single line of code
-name,age,attractive="Aman",22,True
-print(name,age,attractive)
+name, age, attractive = "Aman", 22, True
+print(name, age, attractive)
---
+++ string methods.py
@@ -0,0 +1,11 @@
+name = "aman"
+print(len(name))
+print(name.find("A")) #find is used to get the index of our letter its also case sensitive
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+print(name.isdigit()) #it returns boolean it checks weather the variable caries digit
+print(name.isalpha())#it returns boolean it checks weather the variable caries alphabet
+print(name.count)
+print(name.replace("a","e")) #not case sensitive
+print(name*5)
