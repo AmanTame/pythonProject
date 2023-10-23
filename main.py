82d3808a943cf82a35dd1aadba17b5f6ddd61fea
Dell <trustever.jc@gmail.com>
2023-10-22 22:26:51 -0700

    Working With Variables

---
+++ main.py
@@ -0,0 +1,11 @@
+#variables in python
+print("this is 12 hour course on yt")
+name = "Bro"    #string
+age = 22    #integer
+height = 172.5 #floating
+human = True #boolean
+print("Hello " + name)
+print(type(name))
+print("Your age is: " + str(age))
+print("your height " + str(height) + "cm")
+print("Are you human: "+str(human))
