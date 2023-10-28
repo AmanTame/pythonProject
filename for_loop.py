73a09f7c1ddef71f3a1ee00bd601c6f4bd182569
Dell <trustever.jc@gmail.com>
2023-10-28 08:25:12 -0700

    for loop run and check each block separately

---
+++ for_loop.py
@@ -0,0 +1,13 @@
+import time
+#for_loop is for limited amt of times
+import  time
+for i in range(10+1):
+    print(i)
+for i in range(50,100,3):
+    print(i+1)
+for i in "Aman Tame":
+    print(i)
+for seconds in range(10,0,-1):
+    print(seconds)
+    time.sleep(1)
+print("Happy Birthday!")
