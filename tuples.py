96b34e4bf3a8dd570bbead8b180f02defb3b0fe6
Dell <trustever.jc@gmail.com>
2023-10-30 23:32:55 -0700

    Tuples in python a very quick example!

---
+++ tuples.py
@@ -0,0 +1,11 @@
+#tuples contain  ordered and unchangeable data
+student = ("Aman", "21", "male")
+
+print(student.count("Aman"))
+print(student.index("Aman"))
+
+for i in student:
+    print(i," ",end="")
+
+if "Aman" in student:
+    print("Aman is here!")
