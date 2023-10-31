43ed083a63fa1a27ff286b90a45b983ea687a9ce
Dell <trustever.jc@gmail.com>
2023-10-31 02:55:02 -0700

    indexing in python

---
+++ indexing_in_python.py
@@ -0,0 +1,9 @@
+name = "aman Tame"
+firstname = name[0].capitalize()
+print(firstname, name[1:4])
+firstname = name[:4].upper()
+print(firstname)
+lastname = name[5:].lower()
+print(lastname)
+last_charachter = name[-1]
+print(last_charachter)
