97b81ef3fb5951eda36675fa01f5aeaffc123d10
Dell <trustever.jc@gmail.com>
2023-11-04 06:48:30 -0700

    nested functions

---
+++ nested function calls.py
@@ -0,0 +1,10 @@
+#num = input("Enter a positive no")
+#num = float(num)
+#num = abs(num)
+#num = round(num)
+#print(num)
+
+#The shortest way to do this
+print(round(abs(float(input("Enter a positive no")))))
+
+
