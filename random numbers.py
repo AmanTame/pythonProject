6ef7a5368a3c468ca4bb5ce45e75d1edccdc6945
Dell <trustever.jc@gmail.com>
2023-11-04 20:38:56 -0700

    random numbers in python

--- random numbers.py
+++ random numbers.py
@@ -1,6 +1,12 @@
 import random
 
-x=random.randint(1,6) #displays random no b/n 0 and 6
-y=random.random()   # displays random no b/n 0and 1
+x = random.randint(1,6) #displays random no b/n 0 and 6
+y = random.random()   # displays random no b/n 0and 1
+myList = ["A", "B", "C"]
 print(x)
 print(y)
+z = random.choice(myList)
+print(z)
+cards = [1, 2, 3, 4, 5, "A", "Q", "Z"]
+random.shuffle(cards)
+print(cards)
